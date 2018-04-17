200bb45fb7186063b873edd98a6f4abae7b1de80
Tarcisio.Nunes <tarcisio.nunes@MacBook-Pro.local>
2018-04-17 14:20:42 -0300

    ajustes na logica de migracao

--- src/controle/gestor_controles.py
+++ src/controle/gestor_controles.py
@@ -25,7 +25,6 @@ def criar_etapa_simples(processo, nome, descricao, executor, objetos_afetados, a
     processo.etapas.append(etapa_simples)
     processo.save()
 
-
 def criar_etapa_faseada(processo, nome, descricao, executor, objetos_afetados, atributo_modificado=None):
     etapa_faseada = EtapaFaseada(nome=nome, descricao=descricao, executor=executor, atributo_modificado=atributo_modificado)
     fase = Fase(objetos_afetados=objetos_afetados)
@@ -33,8 +32,25 @@ def criar_etapa_faseada(processo, nome, descricao, executor, objetos_afetados, a
     processo.etapas.append(etapa_faseada)
     processo.save()
 
-
 def adicionar_fase(processo, etapa_faseada, objetos_afetados):
     fase = Fase(objetos_afetados=objetos_afetados)
     etapa_faseada.fases.append(fase)
     processo.save()
+
+if __name__ == '__main__':
+    connect(db='zabbix_tool_staging', host='zabbixtool-01-152389458497.qa2.mongodb.globoi.com', port=27017,
+            username='u_zabbix_tool_st', password='jrwZXcpCBw')
+
+    criar_processo(nome='processo-1', descricao='descricao processo 1', autor='autor processo 1')
+    adicionar_host_controle([{'name': 'host-2', 'hostid': '2'}])
+
+    nome = 'etapa-1'
+    descricao = 'descricao etapa 1'
+    executor = 'executor etapa 1'
+    processo = Processo.objects(nome='processo-1').first()
+    objetos_afetados = Host.objects()
+
+    atributo_incluido = AtributoIncluido(nome='groups', valor='grupo-1', id_atributo='1')
+
+    criar_etapa_simples(processo=processo, nome=nome,
+                                         descricao=descricao, executor=executor, objetos_afetados=objetos_afetados, atributo_modificado=atributo_incluido)
--- src/zabbix/base.py
+++ src/zabbix/base.py
@@ -7,7 +7,24 @@ def find_group_by_name(zabbix_api, name):
     return groups[0]
 
 
-def find_hosts_by_groupid(zapi, groupids):
-    hosts = zapi.host.get(output=['hostid', 'name'], groupids=groupids)
+def find_hosts_by_groupid(zapi, groupids, hostnames=None):
+    if hostnames:
+        hosts = zapi.host.get(output=['hostid', 'name'], groupids=groupids, filter={'host': hostnames})
+    else:
+        hosts = zapi.host.get(output=['hostid', 'name'], groupids=groupids)
 
     return hosts
+
+
+def find_hosts_by_hostnames(zapi, hostnames):
+    hosts = zapi.host.get(output=['hostid', 'name'], filter={'host': hostnames})
+
+    return hosts
+
+
+def mass_add_groups(zapi, groups, hosts):
+    zapi.hostgroup.massadd(groups=groups, hosts=hosts)
+
+
+def mass_remove_groups(zapi, groupids, hostids):
+    zapi.hostgroup.massremove(groupids=groupids, hostids=hostids)
