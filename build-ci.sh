d1b8ffa46e74db810269d20acb0861f0228d22f5
Tarcisio Nunes <tarcisio.nunes@corp.globo.com>
2018-04-24 10:12:10 -0300

    ativando venv no script de ci

--- build-ci.sh
+++ build-ci.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/sh
 
+if [ -n "${WORKSPACE:+1}" ]; then
+    # Path to virtualenv cmd installed by pip
+    # /usr/local/bin/virtualenv
+    PATH=$WORKSPACE/venv/bin:/usr/local/bin:$PATH
+    if [ ! -d "venv" ]; then
+            virtualenv venv
+    fi
+    . venv/bin/activate
+
+fi
+
 sh build.sh
 
 docker-compose -f docker-compose-ci.yml up -d
